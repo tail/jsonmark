17c133617b40772671f802b3fdbbb341abee3380
Jason Yan <tailofthesun@gmail.com>
2022-01-26 19:39:35 -0800

    jsonmark: update java implementation

--- JSONMark/deserializers/java/src/main/java/im/tail/jsonmark/deserializer/App.java
+++ JSONMark/deserializers/java/src/main/java/im/tail/jsonmark/deserializer/App.java
@@ -1,104 +1,57 @@
 package im.tail.jsonmark.deserializer;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
-import java.util.Date;
 
-import com.google.common.io.LineProcessor;
-
-import io.vertx.core.Vertx;
-import io.vertx.core.file.AsyncFile;
-import io.vertx.core.file.FileSystem;
-import io.vertx.core.file.OpenOptions;
-import io.vertx.core.parsetools.RecordParser;
+import com.alibaba.fastjson.JSONObject;
 
 public class App {
-    private static Vertx vertx;
-    private static boolean done;
-
-    public static class MyLineProcessor implements LineProcessor<Void> {
-        public boolean processLine(String line) throws IOException {
-            return true;
-        }
-
-        public Void getResult() {
-            return null;
-        }
-
-    }
+    public static int checksum = 0;
 
     public static void main(String[] args) {
-        done = false;
-        if (args.length != 2) {
-            System.out.println("requires two args: <benchmark> <filename>");
+        if (args.length != 1) {
+            System.out.println("requires one arg: <filename>");
             System.exit(1);
         }
 
-        // Files.readLines using LineProcessor
-        //
-        // var file = new File(args[1]);
-        // try {
-        //     // Files.readLines(file, Charsets.UTF_8, new MyLineProcessor());
-        //     var charSource = Files.asCharSource(file, Charsets.UTF_8);
-        //     charSource.readLines(new MyLineProcessor());
-        // } catch (IOException ex) {
-        //     System.out.println("Got exception: " + ex.toString());
-        //     System.exit(1);
-        // }
+        // NOTE: BufferedReader and Files.lines perform similar (~0.6s for 1M
+        // record 333MiB file).  com.google.common.io.LineProcessor and vertx
+        // were both even slower.
 
         // Using BufferedReader
         //
-        // var file = new File(args[1]);
-        // String line;
-        // try {
-        //     var fr = new FileReader(file);
-        //     var br = new BufferedReader(fr);
-        //     int i = 0;
-        //     while ((line = br.readLine()) != null) {
-        //         i += 1;
-        //         if (i % 100000 == 0) {
-        //             // System.gc();
-        //         }
-        //     }
-        //     br.close();
-        // } catch (FileNotFoundException ex) {
-        //     System.out.println("Got exception: " + ex.toString());
-        //     System.exit(1);
-        // } catch(IOException ex) {
-        //     System.out.println("Got exception: " + ex.toString());
-        //     System.exit(1);
-        // }
-
-        System.out.println(new Date() + " Starting");
-        vertx = Vertx.vertx();
-        FileSystem fs = vertx.fileSystem();
-        RecordParser recordParser = RecordParser.newDelimited("\n", bufferedLine -> {
-            // System.out.println("line = " + bufferedLine);
-        });
-        AsyncFile asyncFile = fs.openBlocking(args[1], new OpenOptions().setRead(true));
-        asyncFile.handler(recordParser)
-            .endHandler(v -> {
-                asyncFile.close();
-                vertx.close();
-                System.out.println(new Date() + " Done!");
-            });
-
-        System.out.println("Waiting to finish...");
-
-        
+        var file = new File(args[0]);
+        String line;
+        try {
+            var fr = new FileReader(file);
+            var br = new BufferedReader(fr);
+            while ((line = br.readLine()) != null) {
+                var obj = JSONObject.parse(line);
+                checksum += JSONObject.class.cast(obj).getInteger("integer_1") + JSONObject.class.cast(obj).getInteger("integer_2");
+            }
+            br.close();
+        } catch (IOException ex) {
+            System.out.println("Got exception: " + ex.toString());
+            System.exit(1);
+        }
 
         // Using Files.lines
         //
-        // var path = Paths.get(args[1]);
+        // var path = Paths.get(args[0]);
         // try {
         //     var lines = Files.lines(path);
         //     lines.forEach(s -> {
-        //         // var obj = JSONObject.parse(s);
-        //         // System.out.println(JSONObject.class.cast(obj).getInteger("integer_1"));
+        //         var obj = JSONObject.parse(s);
+        //         checksum += JSONObject.class.cast(obj).getInteger("integer_1") + JSONObject.class.cast(obj).getInteger("integer_2");
         //     });
         //     lines.close();
         // } catch(IOException ex) {
         //     System.out.println("IOException: " + ex.toString());
         //     System.exit(1);
         // }
+
+        System.out.println(checksum);
     }
 }
